217dea10d357b74747e84be6394f38f1b8519037
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-02-05 13:59:45 -0300

    example custom hooks

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 
 // import { Container } from './styles';
 
+import useAPI from "./Hooks/todos";
+
 export default function Hooks() {
+  const todos = useAPI("https://todos-dsequjaojf.now.sh/todos");
+
   return (
-    <div>
-      <p>Hooks!</p>
-    </div>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
   );
 }
