2cbefc22234979e9a504af1cd69fc289ab59a6af
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-02-05 09:46:08 -0300

    useStaten optimized with useCallback

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 // import { Container } from './styles';
 
 export default function Hooks() {
+  const [time, setTime] = useState();
+  const [count, setCount] = useState(0);
+
+  const inc = useCallback(
+    function handleIncrementCoung() {
+      setCount(prevCount => prevCount + 1);
+    },
+    [setCount]
+  );
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const currentTime = JSON.stringify(new Date(Date.now()));
+      setTime(currentTime);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [time]);
+
   return (
     <div>
       <p>Hooks!</p>
+      <p>The current time is: {time}</p>
+      <p>Count: {count}</p>
+      <button onClick={inc}>+</button>
     </div>
   );
 }
